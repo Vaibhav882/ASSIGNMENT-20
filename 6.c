77a0a9eab9c0888ab58a265b4563b59b3be1dedc
Vaibhav Tiwari <107205805+Vaibhav882@users.noreply.github.com>
2025-10-30 13:11:31 +0530

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100], *ptr;
+    int length = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    ptr = str;
+    while (*ptr != '\0')
+    {
+        length++;
+        ptr++;
+    }
+
+    printf("Length of string: %d\n", length);
+    return 0;
+}
